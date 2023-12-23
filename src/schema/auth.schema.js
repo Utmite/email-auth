56813dbe7c3e2f711933d8e9b0289412bc972262
Utmite <rockimunoz84@gmail.com>
2023-12-23 12:53:34 -0300

    regex

--- src/schema/auth.schema.js
+++ src/schema/auth.schema.js
@@ -1,7 +1,7 @@
 import Joi from "joi"
 
 export const usuarioRegisterSchema = Joi.object({
-    email: Joi.string().email().required().regex(/\uc\.cl$/),
+    email: Joi.string().email().required()
 })
 
 export const usuarioVerifycodeSchema = Joi.object({
